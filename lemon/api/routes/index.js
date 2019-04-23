e88450b8b71cace6ba129df1cf5d73ca58095d04
mqf <452926729@qq.com>
2019-04-23 17:18:16 +0800

    账单接口

--- lemon/api/routes/index.js
+++ lemon/api/routes/index.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var mongo=require("mongodb-curd");
+
+
+var db="1701Blemon";
+var user="user";
+var bill = "bill";
+var classify = "classify";
+var icon = "icon";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-var db="1701Blemon";
-var user="user"
-
 
 //登录接口
+/*
+*关键：登录要查询数据库里面的当前这个数据，把登录成功后的唯一_id返回给用户，用户接收到操作
+ */
 router.post('/api/login', function(req, res, next) {
   var name = req.body.username;
   var password = req.body.password;
@@ -23,14 +31,13 @@ router.post('/api/login', function(req, res, next) {
     }else{
       if(result.length > 0){
         /*
-        *筛选出需要返回的数据
+        *为了用户的安全，把密码给过滤掉
+        *筛选出需要返回的数据,
          */
         let obj = {
           id:result[0]._id,
           name:result[0].name
         }
-
-
         res.json({
           code:1,
           msg:"success",
@@ -46,4 +53,56 @@ router.post('/api/login', function(req, res, next) {
     }
   })
 });
+
+
+/* 根据用户获取当前的账单 */
+/*
+*关键：要根据用户的唯一ID号来查询账单，对应uid
+ */
+router.post('/api/getBill', function(req, res, next) {
+  let uid = req.body.uid;
+  mongo.find(db,bill,{"uid":uid},function(result){
+    if(!result){
+      res.json({
+        code:0,
+        msg:"error"
+      })
+    }else{
+      res.json({
+        code:1,
+        msg:"ok",
+        data:result
+      })
+    }
+  })
+});
+
+
+/* 删除账单 */
+/*
+*关键：根据数据的唯一 “_id” 来删除数据，删除就是使用唯一的数据_id 
+ */
+router.post('/api/deltBill', function(req, res, next) {
+  let id = req.body.id;
+  mongo.remove(db,bill,{"_id":id},function(result){
+    if(!result){
+      res.json({
+        code:0,
+        msg:"删除失败！"
+      })
+    }else{
+      res.json({
+        code:1,
+        msg:"删除成功！",
+        data:result
+      })
+    }
+  })
+
+});
+
+
+
+
+
 module.exports = router;
